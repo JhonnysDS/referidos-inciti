349dee6cbefe1b1b014a7aa0ae11d31003a0bbe9
JhonnysDS <jdeavilasalgado@gmail.com>
2022-07-20 12:40:43 -0500

    Cambio DE URI a la De Heroku

--- config/dev.py
+++ config/dev.py
@@ -4,4 +4,5 @@ from .default import *
 
 APP_ENV = APP_ENV_DEVELOPMENT
 
-SQLALCHEMY_DATABASE_URI = 'mysql://b9734c8de48310:61c5f84f@us-cdbr-east-06.cleardb.net/heroku_4dc8ce8c6973758?reconnect=true'
+SQLALCHEMY_DATABASE_URI = 'mysql://b9734c8de48310:61c5f84f@us-cdbr-east-06.cleardb.net/heroku_4dc8ce8c6973758'
+
--- config/prod.py
+++ config/prod.py
@@ -6,4 +6,4 @@ SECRET_KEY = '5e04a4955d8878191923e86fe6a0dfb24edb226c87d6c7787f35ba4698afc86e95
 
 APP_ENV = APP_ENV_PRODUCTION
 #URI DE HEROKU ↓
-SQLALCHEMY_DATABASE_URI = 'mysql://b9734c8de48310:61c5f84f@us-cdbr-east-06.cleardb.net/heroku_4dc8ce8c6973758?reconnect=true'
+SQLALCHEMY_DATABASE_URI = 'mysql://b9734c8de48310:61c5f84f@us-cdbr-east-06.cleardb.net/heroku_4dc8ce8c6973758'
