a41d18b9096f79c472c39b7ff740d39d4e7583d5
JhonnysDS <jdeavilasalgado@gmail.com>
2022-07-20 13:17:31 -0500

    Cambio DE URI a la De Heroku

--- instance/config-testing.py
+++ instance/config-testing.py
@@ -1,3 +1,2 @@
 # instance/config-testing.py
-
-SQLALCHEMY_DATABASE_URI = 'mysql://b9734c8de48310:61c5f84f@us-cdbr-east-06.cleardb.net/heroku_4dc8ce8c6973758?reconnect=true'
+SQLALCHEMY_DATABASE_URI = 'mysql://b9734c8de48310:61c5f84f@us-cdbr-east-06.cleardb.net/heroku_4dc8ce8c6973758'
