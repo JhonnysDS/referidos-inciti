46f8b501ee436aec8a71e47e93586b4c8c1b9937
JhonnysDS <jdeavilasalgado@gmail.com>
2022-07-24 13:29:27 -0500

    models

--- app/auth/models.py
+++ app/auth/models.py
@@ -6,7 +6,7 @@ from app import db
 class User(db.Model, UserMixin):
     __tablename__ = 'app_user'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     names = db.Column(db.String(80), nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(256), nullable=False)
